247dac604e8df30fb983df36043febdf59f2543e
zhangc <1417164870@qq.com>
2023-05-22 16:51:25 +0800

    代码架子调整

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysPost.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysPost.java
@@ -3,6 +3,9 @@ package com.ruoyi.system.domain;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
@@ -20,6 +23,7 @@ public class SysPost extends BaseEntity
 
     /** 岗位序号 */
     @Excel(name = "岗位序号", cellType = ColumnType.NUMERIC)
+    @TableId(type = IdType.ASSIGN_UUID)
     private Long postId;
 
     /** 岗位编码 */
